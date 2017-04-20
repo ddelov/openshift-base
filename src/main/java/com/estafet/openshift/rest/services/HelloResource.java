6c6d7048c1907fd9e1e1a4f3773fef7b5cf2ebf2
Delcho Delov <delcho.delov@gmail.com>
2017-04-20 20:10:43 +0300

    try to return working version

--- src/main/java/com/estafet/openshift/rest/services/HelloResource.java
+++ src/main/java/com/estafet/openshift/rest/services/HelloResource.java
@@ -6,13 +6,14 @@ import javax.ws.rs.Produces;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
-@Path("/device")
 public class HelloResource {
 
-	@GET
-	@Produces(APPLICATION_JSON)
-	public String ping(){
+		@GET
+		@Path("/device")
+		@Produces(APPLICATION_JSON)
+		public String ping(){
 
-			return "Hello World!";
-	}
+				return "Hello World!";
+		}
+
 }
